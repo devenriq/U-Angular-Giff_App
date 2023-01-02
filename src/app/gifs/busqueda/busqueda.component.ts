43568c756a8c46201cba51a52a81b441ba4153fe
Enrique <octepc.97@gmail.com>
2023-01-02 10:09:11 -0500

    made the sidebar logic. Now it renders whatever is write in the searchbar

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,12 +8,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class BusquedaComponent {
   @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService){}
 
   search(){
     const valor = this.txtBuscar.nativeElement.value
-    console.log(valor)
 
-    this.txtBuscar.nativeElement.value = ''
+    this.gifsService.buscarGifs(valor)
+
+    this.txtBuscar.nativeElement.value = ' '
   }
 
 }
