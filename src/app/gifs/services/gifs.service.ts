3f70b8033a4195bf4d56d752a77947810fef4f2f
Enrique <octepc.97@gmail.com>
2023-01-02 12:38:26 -0500

    http module was used to make a fetch to the info that will be used in the app

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,10 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string= 'IIYlL3sOlKdXnziZ3RH2xKRf7cm4wRwF'
   private _historial: string[]=[]
 
   get historial(){
     return [...this._historial]
+  }
+
+  constructor(private http: HttpClient){
+
   }
 
   buscarGifs(query:string=''){
@@ -20,7 +26,10 @@ export class GifsService {
       this._historial = this._historial.splice(0,10)
     }
 
+    this.http.get('https://api.giphy.com/v1/gifs/trending?api_key=IIYlL3sOlKdXnziZ3RH2xKRf7cm4wRwF&q=dragon ball z&limit=10')
+      .subscribe((res:any)=>{
+        console.log(res.data)
+      })
 
-    console.log(this._historial)
   }
 }
