e9fa877aff584943894c7b0acd0e1895071e4561
Enrique <octepc.97@gmail.com>
2023-01-01 19:10:51 -0500

    take the value of an input using a different and strange way

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
 })
 export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+
+  search(){
+    const valor = this.txtBuscar.nativeElement.value
+    console.log(valor)
+
+    this.txtBuscar.nativeElement.value = ''
+  }
 
 }
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
 import { BusquedaComponent } from './busqueda/busqueda.component';
 import { ResultadosComponent } from './resultados/resultados.component';
@@ -16,7 +18,8 @@ import { ResultadosComponent } from './resultados/resultados.component';
     GifsPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class GifsModule { }
