dbedf424e3329c31530194ba979d340a9428965f
enrique palomino <octepc.97@gmail.com>
2023-01-06 12:52:14 -0500

    save the history on the localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -18,6 +18,10 @@ export class GifsService {
 
   constructor(private http: HttpClient){
 
+    if (localStorage.getItem('historial')) {
+      this._historial = JSON.parse(localStorage.getItem('historial')!)
+    }
+
   }
 
   buscarGifs(query:string=''){
@@ -27,6 +31,8 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query)
       this._historial = this._historial.splice(0,10)
+
+      localStorage.setItem('historial',JSON.stringify(this._historial))
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=IIYlL3sOlKdXnziZ3RH2xKRf7cm4wRwF&q=${query}&limit=10`)
